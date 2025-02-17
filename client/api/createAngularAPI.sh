e0fa393fbb0cc78830ec1d14043def01ebf0ead9
Vriza <avriza@anl.gov>
2025-02-17 12:08:21 -0600

    add .gitattributes to enforce lf line endings

--- client/api/createAngularAPI.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd ../../..
-./backend/node_modules/.bin/lb-sdk backend/server/server.js  ./frontend/src/app/shared/sdk
-cd -
--- client/api/createAngularUniversalAPI.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd ../..
-./node_modules/.bin/lb-sdk -d ng2universal server/server.js  ../frontend/src/app/shared/sdk
-cd -
