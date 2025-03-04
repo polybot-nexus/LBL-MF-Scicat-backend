0d0aef3dd3b46e70bec6272fdcff5989b868aaea
Dozgulbas <dozgulbas@anl.gov>
2025-03-04 16:51:18 -0600

    Updates for external connections

--- server/server.js
+++ server/server.js
@@ -155,7 +155,8 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit("started");
-    var baseUrl = app.get("url").replace(/\/$/, "");
+    // var baseUrl = app.get("url").replace(/\/$/, "");
+    const baseUrl = "http://0.0.0.0:3000";
     console.log("Web server listening at: %s", baseUrl);
     if (app.get("loopback-component-explorer")) {
       var explorerPath = app.get("loopback-component-explorer").mountPath;
